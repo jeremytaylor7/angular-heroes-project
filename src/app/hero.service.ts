76ffd88474dea4a579059d90ebb3f00d67f0a0ed
Jeremy <goldplatedmarketer@gmail.com>
2018-04-05 18:01:23 -0700

    have to fix messages

---
+++ src/app/hero.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+getHeroes(): Observable<Hero[]> {
+  return of(HEROES);
+}
+
+
+// getHeroes(): Hero[] {
+//   return HEROES;
+}
+
+
+
